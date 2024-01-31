858b9c9793f23164da5911d9dd1f25a5d5ac8d45
Christa D. Kitcher <christad.kitcher@Christas-MacBook-Pro.local>
2024-01-30 19:00:47 -0500

    Commit for logger.js activity

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,19 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const logger = {
+    logMsg: function() {
+        if (this.errMsg) {
+            console.log(`Error message from : ${this.errMsg}`);
+        }
+    }
+}
+
+const obj1 = {
+    errMsg: 'Obj1 error'
+}
+
+const obj2 = {
+    errMsg: 'Obj2 error'
+}
+
+logger.logMsg.call(obj1)
+logger.logMsg.call(obj2)
